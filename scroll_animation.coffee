f8b7226c798b484fe68a2d96ef54ab59b6430ab4
Jason Webster <jason@metalabdesign.com>
2014-01-15 17:22:53 -0800

    Add requestAnimationFrame shim

--- scroll_animation.coffee
+++ scroll_animation.coffee
@@ -1,3 +1,12 @@
+window.requestAnimationFrame = (->
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          window.oRequestAnimationFrame      ||
+          window.msRequestAnimationFrame     ||
+          (callback) -> window.setTimeout(callback, 1000 / 60)
+)()
+
 class @ScrollAnimation
   windowHeight   = window.innerHeight
   documentHeight = null
--- scroll_animation.js
+++ scroll_animation.js
@@ -2,6 +2,12 @@
 (function() {
   var __slice = [].slice;
 
+  window.requestAnimationFrame = (function() {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+      return window.setTimeout(callback, 1000 / 60);
+    };
+  })();
+
   this.ScrollAnimation = (function() {
     var STATE_ANIMATING, STATE_IDLE, documentHeight, lastTop, run, update, windowHeight;
 
